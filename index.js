dd35091bfa984e97e566729bd41a351bace6b4d7
Steven Price <sprice7287@gmail.com>
2024-06-04 15:28:07 -0400

    init library backend express

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.listen(
+    PORT,
+    () => console.log(`http://localhost:${PORT}`)
+);
+
+app.get('/search/:keyword', (req, res) => {
+
+    const { keyword } = req.params;
+
+
+    res.status(200).send({
+        name: 'name good',
+        author: 'author good',
+        description: 'description good'
+    })
+})
