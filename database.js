46b64894ca35fcfc91685a8bad8d8c747f455cd3
Steven Price <sprice7287@gmail.com>
2024-06-10 20:41:35 -0400

    search api created

--- database.js
+++ database.js
@@ -8,6 +8,7 @@ const pool = createPool({
     connectionLimit: 10
 })
 
+// search query that checks name, author, and description for what the user is trying to search with the keyword
 async function searchQuery(keyword) {
     return new Promise((resolve, reject)=> {
         pool.query("select * from books WHERE MATCH(name, author, description) AGAINST (?)",[keyword], (err, result, fields) =>{
@@ -15,7 +16,12 @@ async function searchQuery(keyword) {
                 console.error(err);
                 reject(err);
             }
-            console.log(result)
+            
+            console.log("Book names searched: ")
+            for(const book of result){
+                console.log(book.name)
+            }
+        
             resolve(result)
         })
     })
--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+var cors = require('cors');
 
 const { searchQuery } = require("./database.js");
 const app = express();
 const PORT = 8080;
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(express.json());
+
 
 app.listen(
     PORT,
@@ -12,12 +34,8 @@ app.listen(
 );
 
 app.get('/search/:keyword', async (req, res) => {
-
     const { keyword } = req.params;
-
     const result = await searchQuery(keyword);
-
-    console.log("result = " + result);
 
     res.status(200).send(result)
 })
